5c26692bb1967cfc5d062185252a91b5d1309e13
Matheus Araujo <mmaximo@genesisahc.com>
2020-09-14 22:12:00 +0100

    Fix Board limits

--- src/TurtleChallenge.Data.Retriever/Dto/Board.cs
+++ src/TurtleChallenge.Data.Retriever/Dto/Board.cs
@@ -5,10 +5,10 @@ namespace TurtleChallenge.Data.Retriever.Dto
 {
     public class Board
     {
-        [Required, Range(1, 1000)]
+        [Required, Range(2, 1000)]
         public int M { get; set; }
 
-        [Required, Range(1, 1000)]
+        [Required, Range(2, 1000)]
         public int N { get; set; }
     }
 }
